760d89cd421209e104bdd9b4f4876532d2cec282
genarLoya <genarodour2104@gmail.com>
2026-06-28 14:45:51 -0600

    reset: removing all old files

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import Profile from "./Profile.tsx";
-
-export default function Header() {
-    return (
-        <header className="col-span-5 flex justify-between bg-light-card-background p-4 rounded-b-common">
-            <Profile />
-        </header>
-    );
-}
--- components/Photo.tsx
+++
@@ -1,9 +0,0 @@
-export default function Photo() {
-    return (
-        <img
-            class="my-6 rounded-full h-[226px] w-auto"
-            src="/photo.png"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-    );
-}
